153b5c3d66ff631cb87e8f5eaca4e1874888dbb1
Ayofe Faheez <ayofefaheez@gmail.com>
2024-01-13 22:51:19 -0800

    mobile responsive updated

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -24,21 +24,21 @@ function Pricing() {
       </div>
             <div className="pricing__exGrid">
         <div className="ex ex--1">
-          <div className="chrome--con"><img src={chrome} alt="chrome-logo" className="chrome--pic ex--pic"/></div>
+          <div className="chrome--con ex--con"><img src={chrome} alt="chrome-logo" className="chrome--pic ex--pic"/></div>
           <h2> Add to Chrome</h2>
           <p> Minimum version 62</p>  
           <div className="dotted--line"><img src={lineDots} alt="dotted line" className="dotted--line__pic" /></div>
           <div className="ex--btn">Add & Install Extension</div>
         </div>
         <div className="ex ex--2">
-          <div className="firefox--con"><img src={firefox} alt="firefox-logo" className="firefox--pic ex--pic"/></div>
+          <div className="firefox--con ex--con"><img src={firefox} alt="firefox-logo" className="firefox--pic ex--pic"/></div>
           <h2>Add to firefox</h2>
           <p>Minimum version 55</p>
           <div className="dotted--line"><img src={lineDots} alt="dotted line" className="dotted--line__pic" /></div>
           <div className="ex--btn">Add & Install Extension</div>
         </div>
         <div className="ex ex--3">
-        <div className="firefox--con"><img src={opera} alt="opera-logo" className="opera--pic ex--pic"/></div>
+        <div className="opera--con ex--con"><img src={opera} alt="opera-logo" className="opera--pic ex--pic"/></div>
         <h2>Add to Opera</h2>
         <p>Minimum version 46</p>
         <div className="dotted--line"><img src={lineDots} alt="dotted line" className="dotted--line__pic" /></div>
